47047b2c8d26af70e837331ebb8f7c57c0c73dfe
Thomas Deniau <thomas@deniau.org>
2010-04-10 16:33:29 +0200

    Automate DMG creation

---
+++ prepdmg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -rf dmg Hesperides*.dmg
+mkdir dmg
+mkdir dmg/Fonts
+cp dmg-source/DMG_DS_Store dmg/.DS_Store
+cp dmg-source/back.jpg dmg/
+cp TengwarQuenya.ttf TengwarSindarin.ttf dmg/Fonts
+/Developer/Tools/SetFile -a V dmg/back.jpg
+cp -R build/Release/Hesperides.app dmg/
+hdiutil create -srcfolder dmg -fs HFS+ -volname "Hesperides $1" -format UDBZ Hesperides-$1.dmg
+hdiutil attach Hesperides-$1.dmg
