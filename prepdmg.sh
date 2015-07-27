76878c66e6b848ae640c6d5639e51f24d045300a
Thomas Deniau <thomas@deniau.org>
2015-07-27 04:10:56 +0200

    Update the DMG script to be runnable as part of Xcode Archive

--- prepdmg.sh
+++ prepdmg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-rm -rf dmg Hesperides*.dmg
-mkdir dmg
-mkdir dmg/Fonts
-cp dmg-source/DMG_DS_Store dmg/.DS_Store
-cp dmg-source/back.jpg dmg/
-cp TengwarQuenya.ttf TengwarSindarin.ttf dmg/Fonts
-/Developer/Tools/SetFile -a V dmg/back.jpg
-xcodebuild -configuration Release
-cp -R build/Release/Hesperides.app dmg/
-hdiutil create -srcfolder dmg -fs HFS+ -volname "Hesperides $1" -format UDBZ Hesperides-$1.dmg
-hdiutil attach Hesperides-$1.dmg
+rm -rf ${DSTROOT}/dmg ${DSTROOT}/Hesperides*.dmg
+mkdir ${DSTROOT}
+mkdir ${DSTROOT}/dmg
+mkdir ${DSTROOT}/dmg/Fonts
+cp dmg-source/DMG_DS_Store ${DSTROOT}/dmg/.DS_Store
+cp dmg-source/back.jpg ${DSTROOT}/dmg/
+cp TengwarQuenya.ttf TengwarSindarin.ttf ${DSTROOT}/dmg/Fonts
+xcrun SetFile -a V ${DSTROOT}/dmg/back.jpg
+cp -R "${ARCHIVE_PRODUCTS_PATH}/${INSTALL_PATH}/Hesperides.app" ${DSTROOT}/dmg/
+hdiutil create -srcfolder ${DSTROOT}/dmg -fs HFS+ -volname "Hesperides $1" -format UDBZ ${DSTROOT}/Hesperides-$1.dmg
+hdiutil attach ${DSTROOT}/Hesperides-$1.dmg
